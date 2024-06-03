84d89b7b52a7d250f7281e2045cd625df2963701
awesomex <bestawes0mex@gmail.com>
2024-06-03 08:19:15 +0500

    create post migrations, table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,11 @@
-__all__ = ("Base", "Product", "User")
+__all__ = (
+    "Base",
+    "Product",
+    "User",
+    "Post",
+)
 
 from .base import Base
 from .product import Product
 from .user import User
+from .post import Post
