82a84b78a47175e9c542a4d82cbdf94bb3e40e92
awesomex <bestawes0mex@gmail.com>
2024-06-05 14:25:02 +0500

    create relations orders => products

--- core/models/order.py
+++ core/models/order.py
@@ -1,14 +1,21 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
 
 from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+from .order_product_association import order_product_association_table
+
+if TYPE_CHECKING:
+    from .product import Product
 
 
 class Order(Base):
     promocode: Mapped[str | None]
-    price: Mapped[int]
     created_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), default=datetime.now
     )
+    products: Mapped[list["Product"]] = relationship(
+        secondary=order_product_association_table, back_populates="orders"
+    )  # back_populates указывает откуда можно ссылаться на посты
--- core/models/product.py
+++ core/models/product.py
@@ -1,9 +1,19 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
+from typing import TYPE_CHECKING
 
 from .base import Base
+from .order_product_association import order_product_association_table
+
+
+if TYPE_CHECKING:
+    from .order import Order
 
 
 class Product(Base):
     name: Mapped[str]
     price: Mapped[int]
     description: Mapped[str]
+
+    orders: Mapped[list["Order"]] = relationship(
+        secondary=order_product_association_table, back_populates="products"
+    )  # back_populates указывает откуда можно ссылаться на посты
