29ddf04c92e88c3b4a6d111c5d4724175a48ba3a
awesomex <bestawes0mex@gmail.com>
2024-06-05 15:51:22 +0500

    products - orders m2m done

--- core/models/product.py
+++ core/models/product.py
@@ -1,19 +1,22 @@
-from sqlalchemy.orm import Mapped, relationship
 from typing import TYPE_CHECKING
 
-from .base import Base
-from .order_product_association import order_product_association_table
+from sqlalchemy.orm import Mapped, relationship
 
+from .base import Base
 
 if TYPE_CHECKING:
     from .order import Order
+    from .order_product_association import OrderProductAssociation
 
 
 class Product(Base):
     name: Mapped[str]
-    price: Mapped[int]
     description: Mapped[str]
-
+    price: Mapped[int]
     orders: Mapped[list["Order"]] = relationship(
-        secondary=order_product_association_table, back_populates="products"
-    )  # back_populates указывает откуда можно ссылаться на посты
+        secondary="order_product_association",
+        back_populates="products",
+    )
+    orders_details: Mapped[list["OrderProductAssociation"]] = relationship(
+        back_populates="product",
+    )
--- crud_example.py
+++ crud_example.py
@@ -6,9 +6,10 @@ from sqlalchemy.engine import Result
 from sqlalchemy.orm import joinedload, selectinload
 
 from core.db_helper import db_helper
-from core.models import User, Post, Profile
+from core.models import User, Post, Profile, Order, Product, OrderProductAssociation
 
 
+# ВСЕ ЧТО НЕ m2m Связь
 # добавление пользователя
 async def create_user(session: AsyncSession, username: str) -> User:
     user = User(username=username)
@@ -191,14 +192,168 @@ async def main_relations(session: AsyncSession):
     )
 
 
-async def demo_m2m():
-    pass
+# m2m:
+async def create_order(
+    session: AsyncSession,
+    promocode: str | None = None,
+) -> Order:
+    order = Order(promocode=promocode)
+
+    session.add(order)
+    await session.commit()
+
+    return order
+
+
+async def create_product(
+    session: AsyncSession,
+    name: str,
+    description: str,
+    price: int,
+) -> Product:
+    product = Product(
+        name=name,
+        description=description,
+        price=price,
+    )
+    session.add(product)
+    await session.commit()
+    return product
+
+
+async def create_orders_and_products(session: AsyncSession):
+    order_one = await create_order(session)
+    order_promo = await create_order(session, promocode="promo")
+
+    mouse = await create_product(
+        session,
+        "Mouse",
+        "Great gaming mouse",
+        price=123,
+    )
+    keyboard = await create_product(
+        session,
+        "Keyboard",
+        "Great gaming keyboard",
+        price=149,
+    )
+    display = await create_product(
+        session,
+        "Display",
+        "Office display",
+        price=299,
+    )
+
+    order_one = await session.scalar(
+        select(Order)
+        .where(Order.id == order_one.id)
+        .options(
+            selectinload(Order.products),
+        ),
+    )
+    order_promo = await session.scalar(
+        select(Order)
+        .where(Order.id == order_promo.id)
+        .options(
+            selectinload(Order.products),
+        ),
+    )
+
+    order_one.products.append(mouse)
+    order_one.products.append(keyboard)
+    # order_promo.products.append(keyboard)
+    # order_promo.products.append(display)
+
+    order_promo.products = [keyboard, display]
+
+    await session.commit()
+
+
+async def get_orders_with_products(session: AsyncSession) -> list[Order]:
+    stmt = (
+        select(Order)
+        .options(
+            selectinload(Order.products),
+        )
+        .order_by(Order.id)
+    )
+    orders = await session.scalars(stmt)
+
+    return list(orders)
+
+
+async def demo_get_orders_with_products_through_secondary(session: AsyncSession):
+    orders = await get_orders_with_products(session)
+    for order in orders:
+        print(order.id, order.promocode, order.created_at, "products:")
+        for product in order.products:  # type: Product
+            print("-", product.id, product.name, product.price)
+
+
+async def get_orders_with_products_assoc(session: AsyncSession) -> list[Order]:
+    stmt = (
+        select(Order)
+        .options(
+            selectinload(Order.products_details).joinedload(
+                OrderProductAssociation.product
+            ),
+        )
+        .order_by(Order.id)
+    )
+    orders = await session.scalars(stmt)
+
+    return list(orders)
+
+
+async def demo_get_orders_with_products_with_assoc(session: AsyncSession):
+    orders = await get_orders_with_products_assoc(session)
+
+    for order in orders:
+        print(order.id, order.promocode, order.created_at, "products:")
+        for (
+            order_product_details
+        ) in order.products_details:  # type: OrderProductAssociation
+            print(
+                "-",
+                order_product_details.product.id,
+                order_product_details.product.name,
+                order_product_details.product.price,
+                "qty:",
+                order_product_details.count,
+            )
+
+
+async def create_gift_product_for_existing_orders(session: AsyncSession):
+    orders = await get_orders_with_products_assoc(session)
+    gift_product = await create_product(
+        session,
+        name="Gift",
+        description="Gift for you",
+        price=0,
+    )
+    for order in orders:
+        order.products_details.append(
+            OrderProductAssociation(
+                count=1,
+                unit_price=0,
+                product=gift_product,
+            )
+        )
+
+    await session.commit()
+
+
+async def demo_m2m(session: AsyncSession):
+    await create_orders_and_products(session)
+    await demo_get_orders_with_products_through_secondary(session)
+    await demo_get_orders_with_products_with_assoc(session)
+    await create_gift_product_for_existing_orders(session)
 
 
 async def main():
     async with db_helper.section_factory() as session:
         # await main_relations(session)
-        await demo_m2m()
+        await demo_m2m(session)
 
 
 if __name__ == "__main__":
